5f3c6870409bb8af51a68afd8cf899215081a75e
Cedric Krause <ced.krause@gmail.com>
2022-05-06 12:31:45 +0200

    Added BoostrapVue3 as css framework

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(BootstrapVue3)
 
-createApp(App).mount('#app')
+app.mount('#app')
