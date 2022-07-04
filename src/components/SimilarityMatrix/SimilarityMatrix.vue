4beb93f398ed3ba03cfb39265ce3272925d9ce22
Cedric Krause <ced.krause@gmail.com>
2022-07-04 12:24:20 +0200

    Hook similarity matrix up to stores.

--- src/components/SimilarityMatrix/SimilarityMatrix.vue
+++ src/components/SimilarityMatrix/SimilarityMatrix.vue
@@ -2,24 +2,28 @@
   <table>
     <tr>
       <th></th>
-      <th v-for="topic in topics" :key="topic.name">
-        {{ topic.name }}
+      <th v-for="topic in topics" :key="topic.id">
+        {{ topic.id }}
       </th>
     </tr>
-    <tr v-for="outerTopic in topics" :key="outerTopic.name">
+    <tr v-for="outerTopic in topics" :key="outerTopic.id">
       <th class="pr-4">
-        {{ outerTopic.name }}
+        {{ outerTopic.id }}
       </th>
-      <td v-for="innerTopic in topics" :key="innerTopic.name">
+      <td v-for="innerTopic in topics" :key="innerTopic.id">
         <div>
           <svg class="matrix-cell" :viewBox="`0 0 ${width} ${height}`">
             <rect
-              v-for="(period, index) in periods"
+              v-for="(period, index) in innerTopic.periods"
               :key="period.start"
               :x="index * squareSize"
               :width="squareSize"
               :height="height"
-              fill="grey"
+              :fill="
+                similarityScale(
+                  period.similarity - outerTopic.periods[index].threshold
+                )
+              "
             />
           </svg>
         </div>
@@ -29,34 +33,16 @@
 </template>
 
 <script lang="ts" setup>
-  const periods = [
-    {
-      start: '1',
-    },
-    {
-      start: '2',
-    },
-    {
-      start: '3',
-    },
-    {
-      start: '4',
-    },
-  ]
+  import { useDatasetStore } from '@/stores/datasetStore'
+  import * as d3 from 'd3'
+  import { storeToRefs } from 'pinia'
 
-  const topics = [
-    {
-      name: 'abc',
-    },
-    {
-      name: 'def',
-    },
-    {
-      name: 'ghi',
-    },
-  ]
+  const datasetStore = useDatasetStore()
+  const { topics } = storeToRefs(datasetStore)
 
-  const width = 300 / topics.length
+  const width = 300 / datasetStore.periodCount
   const height = 10
-  const squareSize = width / periods.length
+  const squareSize = width / datasetStore.periodCount
+
+  const similarityScale = d3.interpolateGreys
 </script>
