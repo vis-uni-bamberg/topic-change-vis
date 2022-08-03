95d8ebf7b5f2de21f3e898b314a9b57121b9aeac
Cedric Krause <ced.krause@gmail.com>
2022-08-03 09:24:49 +0200

    Added d3 category 10 palette to tailwind color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        palette: {
+          1: '#1f77b4',
+          2: '#ff7f0e',
+          3: '#2ca02c',
+          4: '#d62728',
+          5: '#9467bd',
+          6: '#8c564b',
+          7: '#e377c2',
+          8: '#7f7f7f',
+          9: '#bcbd22',
+          10: '#17becf',
+        },
+      },
+    },
   },
   plugins: [],
 }
