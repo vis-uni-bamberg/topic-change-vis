6c7f51db51209eab0ae5c16a39017092a9ad7114
Cedric Krause <ced.krause@gmail.com>
2022-10-21 11:05:28 +0200

    Extracted SimilarityMatrix Cells with plots into own component.

--- src/components/SimilarityMatrix/SimilarityMatrix.vue
+++ src/components/SimilarityMatrix/SimilarityMatrix.vue
@@ -16,26 +16,10 @@
         class="border-1 border-slate-900"
       >
         <div v-if="outerTopic.id !== innerTopic.id">
-          <svg
-            class="matrix-cell"
-            :viewBox="`0 0 ${width / periodCount} ${height}`"
-          >
-            <rect
-              v-for="(period, index) in innerTopic.periods"
-              :key="period.start"
-              :x="(index * width) / periodCount / periodCount"
-              :width="width / periodCount / periodCount"
-              :height="height"
-              :fill="
-                similarityScale(
-                  similaritiesBetweenTopics[index][outerTopic.id.slice(1)].find(
-                    (similarity) =>
-                      similarity.otherTopicId === innerTopic.id.slice(1)
-                  )?.similarity ?? 0
-                )
-              "
-            />
-          </svg>
+          <SimilarityMatrixPlotCell
+            :topic="innerTopic"
+            :other-topic="outerTopic"
+          />
         </div>
         <div
           v-else
@@ -49,27 +33,9 @@
 
 <script lang="ts" setup>
   import { useDatasetStore } from '@/stores/datasetStore'
-  import { useSimilarityStore } from '@/stores/similarityStore'
-  import * as d3 from 'd3'
   import { storeToRefs } from 'pinia'
+  import SimilarityMatrixPlotCell from './SimilarityMatrixPlotCell.vue'
 
   const datasetStore = useDatasetStore()
-  const { topics, periodCount } = storeToRefs(datasetStore)
-
-  const similarityStore = useSimilarityStore()
-  const { similaritiesBetweenTopics } = storeToRefs(similarityStore)
-
-  // const period = '1'
-  // const topic = '1'
-  // const otherTopic = '2'
-  // console.log(
-  //   similaritiesBetweenTopics.value[period][topic].find(
-  //     (similarity) => similarity.otherTopicId === otherTopic
-  //   )?.similarity
-  // )
-
-  const width = 300
-  const height = 2
-
-  const similarityScale = d3.interpolateGreys
+  const { topics } = storeToRefs(datasetStore)
 </script>
---
+++ src/components/SimilarityMatrix/SimilarityMatrixPlotCell.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg class="matrix-cell" :viewBox="`0 0 ${width / periodCount} ${height}`">
+    <rect
+      v-for="(period, index) in topic.periods"
+      :key="period.start"
+      :x="(index * width) / periodCount / periodCount"
+      :width="width / periodCount / periodCount"
+      :height="height"
+      :fill="
+        similarityScale(
+          similaritiesBetweenTopics[index][otherTopic.id.slice(1)].find(
+            (similarity) => similarity.otherTopicId === topic.id.slice(1)
+          )?.similarity ?? 0
+        )
+      "
+    />
+  </svg>
+</template>
+<script setup lang="ts">
+  import { Topic } from '@/models/Topic'
+  import { useDatasetStore } from '@/stores/datasetStore'
+  import { useSimilarityStore } from '@/stores/similarityStore'
+  import * as d3 from 'd3'
+  import { storeToRefs } from 'pinia'
+
+  const similarityStore = useSimilarityStore()
+  const { similaritiesBetweenTopics } = storeToRefs(similarityStore)
+
+  const datasetStore = useDatasetStore()
+  const { periodCount } = storeToRefs(datasetStore)
+
+  defineProps<{
+    topic: Topic
+    otherTopic: Topic
+  }>()
+
+  const width = 300
+  const height = 2
+
+  const similarityScale = d3.interpolateGreys
+</script>
