fc32b4e60f8c6c8b4b86e00b1a52a9b1aef1a988
Cedric Krause <ced.krause@gmail.com>
2022-11-11 13:45:16 +0100

    Changed colorscale to avoid duplicates and grey.

--- src/stores/datasetStore.ts
+++ src/stores/datasetStore.ts
@@ -118,8 +118,29 @@ export const useDatasetStore = defineStore('datasetStore', {
     ],
     periodCount: (state) => state.topics[0].periods.length,
     colorScale: (state) =>
-      d3
-        .scaleOrdinal(d3.schemeCategory10)
-        .domain(state.topics.map((topic) => topic.id)),
+      d3.scaleOrdinal(colorList).domain(state.topics.map((topic) => topic.id)),
   },
 })
+
+const colorList = [
+  '#1F77B4',
+  '#FF7F0E',
+  '#2CA02C',
+  '#D62728',
+  '#9467BD',
+  '#8C564B',
+  '#E377C2',
+  // '#7F7F7F',
+  '#BCBD22',
+  '#17BECF',
+  '#AEC7E8',
+  '#FFBB78',
+  '#98DF8A',
+  '#FF9896',
+  '#C5B0D5',
+  '#C49C94',
+  '#F7B6D2',
+  '#C7C7C7',
+  '#DBDB8D',
+  '#9EDAE5',
+]
